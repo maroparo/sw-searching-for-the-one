a454f7b0b04cbf4ea4b3ea9c781e74782cc98923
Marlind  Parllaku <marlindparllaku@Marlinds-MBP.lan>
2020-06-20 19:59:23 +0200

    Updates

---
+++ precache-manifest.b2543c5d47769cb71116db0e11be3fd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d9f2edc975892aaa8de740be0dc7a9",
+    "url": "/sw-searching-for-the-one/index.html"
+  },
+  {
+    "revision": "f0e4a5ee0a0fc05305cf",
+    "url": "/sw-searching-for-the-one/static/js/2.a5d90187.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sw-searching-for-the-one/static/js/2.a5d90187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1708d39b7b4b22e0d304",
+    "url": "/sw-searching-for-the-one/static/js/main.4c979c8f.chunk.js"
+  },
+  {
+    "revision": "c8d092cb0d781949c3fa",
+    "url": "/sw-searching-for-the-one/static/js/runtime-main.aa521992.js"
+  },
+  {
+    "revision": "a7c35735cc313949cd60e7ae0ce21150",
+    "url": "/sw-searching-for-the-one/static/media/logo.a7c35735.png"
+  },
+  {
+    "revision": "05255a615433439538aef2675b1e432d",
+    "url": "/sw-searching-for-the-one/static/media/soloist.05255a61.otf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/sw-searching-for-the-one/static/media/star-jedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "a04075d2d1d2591fda87c075821f45b3",
+    "url": "/sw-searching-for-the-one/static/media/star-wars-background.a04075d2.jpg"
+  }
+]);
